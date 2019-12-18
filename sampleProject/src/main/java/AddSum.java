b9c26f6abfc6ab97ef28477cae6fbca3a4b67d13
Mugundhan <58930635+Mugundhanp2497@users.noreply.github.com>
2019-12-18 17:29:05 +0530

    Initial commit

---
+++ sampleProject/src/main/java/AddSum.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+
+public class AddSum
+{
+    public static void main(String[] args)
+    {
+      int Rno=1;
+      String Name="mugundhan";
+      System.out.println(Rno);
+      System.out.println(Name);
+
+    }
+}
